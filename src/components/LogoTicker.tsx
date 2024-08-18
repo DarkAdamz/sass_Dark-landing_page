f0d94aaf63a7b72479acffdf95629d0ec8f66c37
DarkAdamz <agwuuchea@gmail.com>
2024-08-18 13:32:42 +0100

    Forgot to add something

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -22,7 +22,7 @@ export const LogoTicker = () => {
         <>
             <div className="bg-black text-white py-[72px] sm:py-24 ">
                 <div className="container">
-                    <h2 className="text-xl text-center text-white/60">Trusted by the world's most innovative Team.</h2>
+                    <h2 className="text-xl text-center text-white/60">Trusted by the most innovative Team.</h2>
 
                     <div className="overflow-hidden mt-9 before:content-[' '] after:contents-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))] ">
                         <motion.div
